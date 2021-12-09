51b0ffbded6145e8b37dbdde8084580df78405d1
halle1120 <91086984+halle1120@users.noreply.github.com>
2021-12-09 11:13:18 -0500

    12/9 Push

---
+++ iTunesMusicAPI.py
@@ -0,0 +1,77 @@
+import requests 
+import json 
+import sys
+import os
+import matplotlib
+import sqlite3
+import unittest
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
+from pprint import pprint
+
+def getAlbumfeatues(artist):
+    base_url = f"https://itunes.apple.com/search?term={artist}"
+    response = requests.get(url=base_url)
+    json_response = response.json()
+    print(json_response)
+
+#gets artists from the title of a song
+def cleanName(name):
+    # split artists by "," and "&", then make sure there are no hanging characters or spaces
+    names = name.replace('&', ',').split(',')
+    for i in range(len(names)):
+        names[i] = names[i].split('[')[0].replace(')', '').replace(']', '').strip() 
+    return names
+
+#get all artists from a song
+def getArtists(song):
+    ret = set(cleanName(song['artistsName']))
+    others = song(['trackName'])
+    if 'feat.' in others:
+        for name in cleanName(others.split('feat.')[1]):
+            ret.add(name)
+    return ret
+
+# generate dictionary with artists and song IDs
+def getData(json):
+    songs = json['results']
+    artists = {}
+    songIDs = {}
+    for song in songs:
+        #GETTING ARTISTS
+        a = getArtists(song)
+        for artist in a:
+            if artist not in artists:
+                artists[artists] = 0
+            artists[artist] += 1
+        
+        # GET SONG / SONG ID
+        songIDs[song['trackName']] = song['trackId']
+    
+    del artists['Drake'] # making sure it's not counting Drake since it's his songs
+
+    return {
+        'artists': artists,
+        'ids': songIDs
+    }
+
+
+
+def main():
+    json = getAlbumfeatues("drake")
+    data = getData(json)
+    '''
+        data = {
+            artsits: {
+                name: count, ...
+            }
+            ids: {
+                title: id, ...
+            }
+        }
+    '''
+    pprint(data)
+
+if __name__ == '__main__':
+    main()
