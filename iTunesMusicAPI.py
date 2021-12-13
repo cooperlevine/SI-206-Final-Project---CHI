2dabbb027f64182df4afd666017551b8ffe397c6
halle1120 <91086984+halle1120@users.noreply.github.com>
2021-12-13 00:03:28 -0500

    set up database

--- iTunesMusicAPI.py
+++ iTunesMusicAPI.py
@@ -6,12 +6,13 @@ import matplotlib
 import sqlite3
 import unittest
 import csv
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 from pprint import pprint
 
 def getAlbumfeatues(artist):
-    base_url = f"https://itunes.apple.com/search?term={artist}&entity=song&attribute=allArtistTerm&limit=89"
+    base_url = f"https://itunes.apple.com/search?term={artist}&entity=song&attribute=allArtistTerm&limit=200"
     response = requests.get(url=base_url)
     json_response = response.json()
     return json_response
@@ -47,17 +48,54 @@ def getData(json):
             artists[artist] += 1
         
         # GET SONG / SONG ID
-        if "feat." in song['trackName'] and 'Drake' not in song['trackName']:
-            songIDs[song['trackName']] = song['trackId']
+        # "feat." in song['trackName'] and 'Drake' not in song['trackName']:
+        songIDs[song['trackName']] = [song['trackId'], song['artistName']]
     
     del artists['Drake'] # making sure it's not counting Drake since it's his songs
-
+    #print(len(songIDs))
     return {
         'artists': artists,
         'ids': songIDs
     }
 
+def setUpDatabase(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn  
 
+def setUpFeatures(data, id_list, cur, conn):
+    artists = []
+    tracks = []
+    ids = []
+    features = []
+    reg_expression = r'(\(.+\))'
+    for track, value in data['ids'].items():
+        tracks.append(track.split('(')[0])
+        artists.append(value[1])
+        ids.append(value[0])
+        if 'feat.' in track:
+            feature = re.findall(reg_expression, track)
+            features.append(feature)
+        else:
+            features.append('None')
+    
+    index = 0
+    for id in ids:
+        if str(id) in id_list:
+            index += 1
+
+    '''
+    try:
+        cur.execute("SELECT track_id FROM Features WHERE track_id = (SELECT MAX(track_id) FROM Features)")
+        start = cur.fetchone()[0] + 1
+    except:
+        start = 0
+    '''
+    for i in range(len(ids))[index:index + 25]:
+        cur.execute("CREATE TABLE IF NOT EXISTS Features (track_id INTEGER PRIMARY KEY, title TEXT UNIQUE, artist TEXT, features TEXT)")
+        cur.execute("INSERT OR IGNORE INTO Features (track_id, title, artist, features) VALUES (?,?,?,?)", (ids[i], tracks[i], artists[i], features[i][0]))
+    conn.commit()
 
 def main():
     json = getAlbumfeatues("drake")
@@ -72,7 +110,22 @@ def main():
             }
         }
     '''
+    cur, conn = setUpDatabase('Features.db')
+    #cur.execute("DROP TABLE IF EXISTS Features")
+
+    id_list = []
+    try:
+        cur.execute("SELECT track_id FROM Features")
+        for row in cur:
+            id_list.append(row[0])
+    except:
+        id_list = []
+    
+
+
+    setUpFeatures(data, id_list, cur, conn)
     pprint(data)
+
 
 if __name__ == '__main__':
     main()
